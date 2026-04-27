4530d2f01fa6b0e2b915eb61ae5ba0fb9e1bc373
HoangBaoo5313 <vhgqbao66@gmail.com>
2026-04-27 23:29:53 +0700

    commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception { // hàm main để chạy và throw một ngoại lệ nếu có lỗi xảy ra
-        Game game999 = new Game(); // tạo một đối tượng game
         game.run(); // gọi phương thức run để chạy trò chơi
     }
 }
