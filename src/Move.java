09353367e9ca4d80a7a34ace65184265f494f24f
dangtram879-glitch <dangtram879@gmail.com>
2026-05-08 10:09:15 +0700

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -17,6 +17,5 @@ public class Move {
     public boolean isAdjacent() {
         int rowDiff = Math.abs(from.getRow() - to.getRow());
         int colDiff = Math.abs(from.getCol() - to.getCol());
-        return (rowDiff == 1 && colDiff ==0) || (rowDiff == 0 && colDiff ==1);
     }
 }
