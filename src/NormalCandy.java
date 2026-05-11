0797361fc1bc048a601efec66f49ef226bc55ca9
dangtram879-glitch <dangtram879@gmail.com>
2026-05-11 17:15:24 +0700

    Update NormalCandy.java

--- src/NormalCandy.java
+++ src/NormalCandy.java
@@ -1,7 +1,3 @@
-/**
- * Class NormalCandy - Standard candy implementation.
- * (Kẹo bình thường - Có thể mở rộng kẹo sọc, kẹo bọc sau này.)
- */
 public class NormalCandy extends Candy {
     public NormalCandy(CandyColor color, int row, int col) {
         super(color, row, col);
@@ -12,7 +8,6 @@ public class NormalCandy extends Candy {
 
     @Override
     public void crush() {
-        // Output for debugging (In ra console để test)
         System.out.println("Crushed candy at [" + row + "][" + col + "]");
     }
 }
